f1ada5566b5daf51b9e2d5bc76a32772705f299c
SeoHee3478 <seohee3478@gmail.com>
2023-11-09 23:43:47 +0900

    refac: jotai 리팩토링

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { useAtom } from "jotai";
+import { themeAtom } from "../data/jotai";
 
-export default function Footer({ isDark, toggleThema }) {
+export default function Footer() {
+  const [theme, setTheme] = useAtom(themeAtom);
+
   return (
-    <footer className={`footer ${isDark ? "dark" : "white"}`}>
-      <button onClick={toggleThema}>테마 변경</button>
+    <footer className={`footer ${theme === "light" ? "white" : "dark"}`}>
+      <button
+        onClick={() =>
+          setTheme((state) => (state === "light" ? "dark" : "light"))
+        }
+      >
+        테마 변경
+      </button>
     </footer>
   );
 }
